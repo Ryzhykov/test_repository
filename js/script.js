08f9fec58d9c316e4ec4cce80b64bd778e51e0b7
Igor <iryzhikov.igor@gmail.com>
2022-02-05 18:15:09 +0200

    first commit.v1

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//console.log('arr' + '- object');
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// // console.log(incr);
+// console.log(--decr);
+// // console.log(decr);
+
+// console.log(5%2);
+
+// console.log (2 === "2");
+
+
+const isChecked = false,
+      isClose = true;
+
+console.log(isChecked || isClose);
